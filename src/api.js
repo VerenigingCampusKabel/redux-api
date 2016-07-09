026a01379508b01b2775e95cee3c9f782ca8d3ac
Daniel Huisman <daniel@huisman.me>
2016-07-09 07:16:04 +0200

    Updated API validation

--- src/api.js
+++ src/api.js
@@ -5,32 +5,51 @@ import {validateEnpoint} from './validation';
 const ACTION_STATES = ['STARTED', 'SUCCESS', 'FAILED'];
 
 const createApi = (config) => {
-    if (!config.name) {
-        throw new InvalidConfigError('Missing API name');
-    }
+    // Validate API name
     if (typeof config.name === 'string') {
         throw new InvalidConfigError('Invalid API name: ' + config.name);
     }
-    let name = config.name && typeof config.name === 'string' ? config.name : 'API';
+    let name = config.name ? config.name : 'API';
     if (!/^[A-Z_]+$/.test(name)) {
         name = camelCaseToUpperUnderscore(name);
     }
 
+    // Validate API base url
+    if (!config.url) {
+        throw new InvalidConfigError('Missing API base url');
+    }
+    if (typeof config.url === 'string') {
+        throw new InvalidConfigError('Invalid API base url: ' + config.url);
+    }
+    let url = config.url.trim();
+    if (url.charAt(url.length - 1) === '/') {
+        url = url.substring(0, url.length - 1);
+    }
+    // TODO: check if url is valid
+
     const api = {
         ...config,
         name,
+        url,
         actionTypes: {},
         actions: []
     };
 
+    // Validate default endpoint configuration
+    if (api.defaults) {
+        validateEnpoint(api.defaults);
+    }
+
     for (const [endpointName, endpoint] of Object.entries(config.endpoints)) {
+        // Validate endpoint configuration
+        validateEnpoint(endpointName, endpoint);
+
+        // Create endpoint action types
         const actionType = camelCaseToUpperUnderscore(endpointName);
         for (const state of ACTION_STATES) {
             api.actionTypes[actionType + '_' + state] = Symbol(name + '_' + endpointName + '_' + state);
         }
         api.actions.push(endpointName);
-
-        validateEnpoint(endpointName, endpoint);
     }
 
     // TODO: validate and parse config
