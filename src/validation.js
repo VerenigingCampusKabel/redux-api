b48ac526425451ce2a9f405c917689ce1df0611c
Daniel Huisman <daniel@huisman.me>
2016-07-11 15:27:57 +0200

    Removed obsolete validation

--- src/validation.js
+++ src/validation.js
@@ -2,12 +2,7 @@ import {InvalidConfigError} from './errors';
 import {getJSON} from './util';
 
 export const VALID_REQUEST_PROPERTIES = ['url', 'method', 'headers', 'query', 'body', 'mode', 'credentials', 'cache', 'redirect', 'referer', 'integrity'];
-export const VALID_RESPONSE_PROPERTIES = ['payload'];
-const VALID_METHOD = ['OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'TRACE', 'CONNECT'];
-const VALID_MODE = ['cors', 'no-cors', 'same-origin'];
-const VALID_CREDENTIALS = ['omit', 'same-origin', 'include'];
-const VALID_CACHE = ['default', 'no-store', 'reload', 'no-cache', 'force-cache', 'only-if-cached'];
-const VALID_REDIRECT = ['follow', 'error', 'manual'];
+export const VALID_RESPONSE_PROPERTIES = ['bailout', 'payload'];
 
 export const validateEnpoint = (endpointName, endpoint, isDefaults = false) => {
     for (const property of Object.keys(endpoint)) {
@@ -31,22 +26,4 @@ export const validateEnpoint = (endpointName, endpoint, isDefaults = false) => {
             endpoint.payload = getJSON;
         }
     }
-
-    // if (Object.keys(other) !== 0) {
-    //     throw new InvalidConfigError(`Invalid endpoint properties (${Object.keys(other).join(', ')}) on ${endpointName}`);
-    // }
-    //
-    // if (!method) {
-    //     throw new InvalidConfigError(`Missing endpoint method on ${endpointName}`);
-    // } else if (typeof method !== 'string' || VALID_METHOD.indexOf(method) === -1) {
-    //     throw new InvalidConfigError(`Invalid endpoint method (${method}) on endpoint ${endpointName}`);
-    // }
-    //
-    // if (headers && typeof headers !== 'object') {
-    //     throw new InvalidConfigError(`Invalid endpoint headers (${headers}) on endpoint ${endpointName}`);
-    // }
-    //
-    // if (query && (typeof query !== 'string' && typeof query !== 'object')) {
-    //     throw new InvalidConfigError(`Invalid endpoint query (${query}) on endpoint ${endpointName}`);
-    // }
 };
