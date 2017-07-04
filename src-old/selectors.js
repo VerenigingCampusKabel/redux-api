0e4f668e2f60ddf44ae083c9c322f6778b691d1a
Daniel Huisman <daniel@huisman.me>
2017-07-04 17:52:03 +0200

    Remove old source directory

--- src-old/selectors.js
+++
@@ -1,7 +0,0 @@
-export const createApiSelectors = (api) => {
-    const models = {};
-    for (const [modelName, model] of Object.entries(api.models)) {
-        models[modelName] = model.selectors;
-    }
-    return models;
-};
